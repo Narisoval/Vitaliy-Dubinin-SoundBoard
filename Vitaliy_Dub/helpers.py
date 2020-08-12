b1baf535a1f3668aa23133d139f02726a8e16c85
Narisoval <60064281+Narisoval@users.noreply.github.com>
2020-08-12 15:22:16 +0300

    Compressed images and audio. Some minor changes

--- Vitaliy_Dub/helpers.py
+++ Vitaliy_Dub/helpers.py
@@ -3,10 +3,10 @@ from kivymd.uix.label import MDLabel
 from kivy.properties import StringProperty
 from kivymd.uix.list import OneLineAvatarListItem
 def change_dict_gachi():
-    dict["phrase4"]["audio"] = SoundLoader.load("./audio/rip-ears.wav")
+    dict["phrase4"]["audio"] = SoundLoader.load("./audio/rip-ears.mp3")
 
 def change_dict_ungachi():
-    dict["phrase4"]["audio"] = SoundLoader.load("./audio/tata.wav")
+    dict["phrase4"]["audio"] = SoundLoader.load("./audio/tata.mp3")
 
 class Item(OneLineAvatarListItem):
     divider = None
@@ -20,11 +20,10 @@ NavigationLayout:
         Screen:
             ScrollView
                 MDGridLayout:
-                    cols:2 
-                	padding : [25,'70dp',30,'5dp']
-                	spacing : ['0.7dp','0.7dp']
+                    cols:2
+                    padding : [25,'60dp',30,30]
+                    spacing : ['1.5dp','1.5dp']
                     id : layout
-                
 
             BoxLayout:
                 orientation: "vertical"
@@ -33,7 +32,7 @@ NavigationLayout:
                     elevation: 10
                     left_action_items: [["menu", lambda x: nav_drawer.set_state()]]
                 Widget:
-            
+
     MDNavigationDrawer:
         id: nav_drawer
         BoxLayout:
@@ -41,70 +40,67 @@ NavigationLayout:
             spacing : '8dp'
             orientation: 'vertical'
             Image:
-                source: './icons/vetal.jpg'
+                source: './icons/vetal.jpeg'
             ScrollView:
                 MDList:
                     id: list
-                
-                                            
-    
-                        
+
      '''
 dict = {
 "phrase1":{
-        "text": "Ходили за пивом", 
-        "audio" : SoundLoader.load("./audio/mi_xodili_za_pivom.wav")},
+        "text": "Ходили за пивом",
+        "audio" : SoundLoader.load("./audio/mi_xodili_za_pivom.mp3")},
 "phrase2": {
         "text":"Бывшые военные",
-        "audio" : SoundLoader.load("./audio/letchiki.wav")},
+        "audio" : SoundLoader.load("./audio/letchiki.mp3")},
 "phrase3": {
             "text" : "Козлы",
-            "audio": SoundLoader.load("./audio/kozli.wav")},
+            "audio": SoundLoader.load("./audio/kozli.mp3")},
 "phrase4": {
             "text" : "Рэп",
-            "audio": SoundLoader.load("./audio/tata.wav")},
+            "audio": SoundLoader.load("./audio/tata.mp3")},
 "phrase5": {
             "text" : "Сколько вам лет?",
-            "audio": SoundLoader.load("./audio/vosrast.wav")},
+            "audio": SoundLoader.load("./audio/vosrast.mp3")},
 "phrase6": {
             "text" : "Я хочу рэп спеть",
-            "audio": SoundLoader.load("./audio/xochu_rep_spet.wav")},
+            "audio": SoundLoader.load("./audio/xochu_rep_spet.mp3")},
 "phrase7": {
            "text" : "Рэп 2",
-            "audio": SoundLoader.load("./audio/rap2.wav")},
+            "audio": SoundLoader.load("./audio/rap2.mp3")},
 "phrase8": {
            "text" : "Очаковское",
-            "audio": SoundLoader.load("./audio/ochakovo.wav")},
+            "audio": SoundLoader.load("./audio/ochakovo.mp3")},
 "phrase9": {
            "text" : "Улица роз",
-            "audio": SoundLoader.load("./audio/ulica_roz.wav")},
+            "audio": SoundLoader.load("./audio/ulica_roz.mp3")},
 "phrase10": {
            "text" : "Коллапс",
-            "audio": SoundLoader.load("./audio/collaps.wav")},
+            "audio": SoundLoader.load("./audio/collaps.mp3")},
 "phrase11": {
            "text" : "Виталик",
-            "audio": SoundLoader.load("./audio/vitalic.wav")},
+            "audio": SoundLoader.load("./audio/vitalic.mp3")},
 "phrase12": {
            "text" : "Апельсин",
-            "audio": SoundLoader.load("./audio/apelcin.wav")},
+            "audio": SoundLoader.load("./audio/apelcin.mp3")},
 "phrase13": {
            "text" : "Стопарь",
-            "audio": SoundLoader.load("./audio/stopar.wav")},           
+            "audio": SoundLoader.load("./audio/stopar.mp3")},
 "phrase14": {
            "text" : "Все время",
-            "audio": SoundLoader.load("./audio/menya_zactavlyt.wav")},
+            "audio": SoundLoader.load("./audio/menya_zactavlyt.mp3")},
 "phrase15": {
            "text" : "Я повязан",
-            "audio": SoundLoader.load("./audio/povyazan.wav")},
+            "audio": SoundLoader.load("./audio/povyazan.mp3")},
 "phrase16": {
            "text" : "Как учили",
-            "audio": SoundLoader.load("./audio/strelbisha.wav")},
+            "audio": SoundLoader.load("./audio/strelbisha.mp3")},
 "phrase17" : {
             "text" : "Телевизор",
-            "audio" : SoundLoader.load("./audio/televizor.wav")},
+            "audio" : SoundLoader.load("./audio/televizor.mp3")},
 "phrase18" : {
             "text" : "Заливается...",
-            "audio" : SoundLoader.load("./audio/vilivayetsa.wav")}
+            "audio" : SoundLoader.load("./audio/vilivayetsa.mp3")}
 
 }
 
--- Vitaliy_Dub/main.py
+++ Vitaliy_Dub/main.py
@@ -1,43 +1,41 @@
 #encoding: utf-8
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.button import MDIconButton 
+from kivymd.uix.button import MDIconButton
 from kivy.core.audio import Sound
 from kivymd.uix.dialog import MDDialog
 from helpers import *
 
 
+
+
 class mainApp(MDApp):
 
     def mybutton(self,num):
-        return MDIconButton(icon=self.main_icon, 
-                            on_press = lambda e: self.play_sound(dict[f"phrase{num}"]["audio"]))
+        return MDIconButton(icon=self.main_icon,
+                            on_press=lambda e: self.play_sound(dict[f"phrase{num}"]["audio"]))
     def build_config(self,config):
         config.setdefaults('section1',{
             'Theme' : 'Light',
              'Icon' : './icons/dd.png'
-        }) 
+        })
     def build(self):
         config = self.config
         self.theme_cls.theme_style = config.get('section1','Theme')
         self.main_icon = config.get('section1','Icon')
         self.theme_cls.primary_palette = 'Yellow'
         self.theme_cls.primary_hue = '700'
-       
         #TEMPS
         self.check_list = []
         self.previous_path = ""
-        return Builder.load_string(helper)                       
-    
-    
+        return Builder.load_string(helper)
+
     def theme_icon(self):
         if self.theme_cls.theme_style == 'Light':
             return './icons/chern.png'
         else:
             return './icons/bel.png'
 
-
-    
     def on_start(self):
         for i in range(1,18,2):
             btn1 = self.mybutton(i)
@@ -48,73 +46,74 @@ class mainApp(MDApp):
             self.root.ids.layout.add_widget(btn2)
             self.root.ids.layout.add_widget(lbl1)
             self.root.ids.layout.add_widget(lbl2)
-      
-        
         #NAV DRAWER(LIST) 
         theme_changer = Item(text= 'Изменить темку', source = self.theme_icon(), on_press = self.change_theme)
         self.icon_changer = Item(text = 'Изменить значок', source = self.main_icon, on_press = self.show_dialog)
-        
-    
+
         self.root.ids.list.add_widget(theme_changer)
         self.root.ids.list.add_widget(self.icon_changer)
-        
+
     def change_theme(self,obj):
         if self.theme_cls.theme_style == 'Light':
             self.theme_cls.theme_style = 'Dark'
         else:
-           self.theme_cls.theme_style = 'Light' 
+           self.theme_cls.theme_style = 'Light'
         self.root.ids.list.clear_widgets()
         theme_changer = Item(text= 'Изменить темку', source = self.theme_icon(), on_press = self.change_theme)
         self.root.ids.list.add_widget(theme_changer)
         self.root.ids.list.add_widget(self.icon_changer)
-        
-    dialog = None    
+
+    dialog = None
     def show_dialog(self, obj):
         self.dialog = MDDialog(
             type="simple",
             size_hint = (.7,.8),
             items=[
-                Item(text = 'Стандартная картинка', source = './icons/dd.png', on_press = lambda e : self.change_icon('./icons/dd.png')),
+                Item(text = 'Стандартная картинка', source = './icons/dd.png', on_press = lambda e : self.checkIcon('./icons/dd.png')),
                 Item(text = 'Виталий Алексеевич', source = './icons/Vetal.png', on_press =  self.vitalic),
-                Item(text = 'Ария лого', source = './icons/Aria.png', on_press = lambda e : self.change_icon('./icons/Aria.png')),
-                Item(text='Знак Анархии', source= './icons/A.png', on_press = lambda e : self.change_icon('./icons/A.png')),
+                Item(text = 'Ария лого', source = './icons/Aria.png', on_press = lambda e : self.checkIcon('./icons/Aria.png')),
+                Item(text='Знак Анархии', source= './icons/A.png', on_press = lambda e : self.checkIcon('./icons/A.png')),
                 Item(text = 'ALT11', source = './icons/male.png', on_press = self.gachi),
-                Item(text = 'Коза', source = './icons/koza.png', on_press = lambda e : self.change_icon('./icons/koza.png')),
-                Item(text = 'Бас', source = './icons/Bass.png', on_press = lambda e : self.change_icon('./icons/Bass.png')),
+                Item(text = 'Коза', source = './icons/koza.png', on_press = lambda e : self.checkIcon('./icons/koza.png')),
+                Item(text = 'Бас', source = './icons/Bass.png', on_press = lambda e : self.checkIcon('./icons/Bass.png')),
                 Item(text = 'Очаково', source = './icons/ochakovo.png', on_press = self.ochakovo)
             ])
-            
-        self.dialog.open()        
-    
-    def change_icon(self,path):
-        if self.previous_path != './icons/male.png':
-            pass
-        else:
-            change_dict_ungachi()
+        self.dialog.open()
+    def change_icon(self,real_path):
         self.root.ids.layout.clear_widgets()
         self.root.ids.list.clear_widgets()
-        self.main_icon = path
+        self.main_icon = real_path
         self.on_start()
         self.dialog.dismiss()
-        self.previous_path = path
+    def checkIcon(self,path):
+        if self.main_icon == path:
+            self.dialog.dismiss()
+            chosen = MDDialog(text = 'Этот значок уже выбран!', radius = [20,7,20,7])
+            chosen.open()
+        elif self.previous_path != './icons/male.png':
+            self.change_icon(path)
+        else:
+            change_dict_ungachi()
+            self.change_icon(path)
+
 
     #SPECIAL CASES
     def gachi(self,obj):
         change_dict_gachi()
-        self.change_icon('./icons/male.png')
+        self.checkIcon('./icons/male.png')
 
     def vitalic(self,obj):
-        self.change_icon('./icons/Vetal.png')
+        self.checkIcon('./icons/Vetal.png')
         dict["phrase11"]["audio"].volume = 0.5
         dict["phrase11"]["audio"].play()
-    
+
+
     def ochakovo(self,obj):
-        self.change_icon('./icons/ochakovo.png')
+        self.checkIcon('./icons/ochakovo.png')
         dict["phrase8"]["audio"].volume = 0.5
         dict["phrase8"]["audio"].play()
 
 
-    
     def play_sound(self, sound):
         self.check_list.append(sound)
         sound.volume = 0.5
@@ -123,20 +122,18 @@ class mainApp(MDApp):
             self.check_list[1].play()
 
             self.check_list.pop(0)
-            
+
         elif len(self.check_list) == 1:
             self.check_list[0].play()
-    def on_pause(self):
-        print("EXECUTING ON PAUSE METHOD!!!")
+    def on_stop(self):
         self.config.setall('section1',{
             'Theme' : self.theme_cls.theme_style,
             'Icon'  : self.main_icon
         })
         with open('main.ini', 'w') as config:
-            self.config.write() 
+            self.config.write()
 
  #ffmpeg -i rap2.wav -ss 0 -to 10 rap2.wav
 
 if __name__ == "__main__":
     mainApp().run()
-
